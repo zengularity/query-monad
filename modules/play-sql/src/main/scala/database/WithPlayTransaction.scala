e3fce798e5601f40b4199e11b94fca75e552de6e
Stéphane TANKOUA <s.tankouat@gmail.com>
2018-05-28 12:34:10 +0200

    Fix Play resource loaner to handle explicitly commit and rollback case

--- modules/play-sql/src/main/scala/database/WithPlayTransaction.scala
+++ modules/play-sql/src/main/scala/database/WithPlayTransaction.scala
@@ -1,6 +1,7 @@
 package com.zengularity.querymonad.module.playsql.database
 
 import java.sql.Connection
+import scala.util.control.ControlThrowable
 
 import play.api.db.Database
 
@@ -8,10 +9,22 @@ import com.zengularity.querymonad.module.sql.WithSqlConnection
 
 class WithPlayTransaction(db: Database) extends WithSqlConnection {
   def apply[A](f: Connection => A): A = {
-    val connection = db.getConnection(true)
-    val result = f(connection)
+    val connection = db.getConnection(false)
+    val result = try {
+      f(connection)
+    } catch {
+      case ex: Throwable =>
+        connection.rollback()
+        throw ex
+    }
     result
   }
 
-  def releaseIfNecessary(connection: Connection): Unit = connection.close()
+  def releaseIfNecessary(connection: Connection): Unit = {
+    try {
+      connection.commit()
+    } finally {
+      connection.close()
+    }
+  }
 }
